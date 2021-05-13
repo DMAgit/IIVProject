e1153c2dc54d2a7f036233d54e6e48929672951c
Dimitar Angelov <44362174+DMAgit@users.noreply.github.com>
2021-05-13 14:46:40 +0300

    Add files via upload

--- plotting.r
+++ plotting.r
@@ -5,10 +5,65 @@ library(dplyr)
 dataIMDB <- read_csv("D:\\Google Drive\\Uni\\Tilburg\\Semester 6\\Interactive Information Visualization\\Group Project\\dataPreprocessed.csv")
 
 dataIMBDclean <- na.omit(dataIMDB)
+
+averageYearGenre_1 <- group_by(dataIMBDclean, year, genre_1) %>% 
+  summarize(average_score = mean(avg_vote)) 
+
 summary(dataIMBDclean)
 
-ggplot(data = dataIMBDclean) + 
-  geom_line(mapping = aes(x = year, y = avg_vote, 
-                          group = genre_1, 
-                          color = genre_1)
+
+
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("IMDB Average Score"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput("genresGroup",
+                  "Choose which genre to display:",
+                  choices = list("Action",
+                                 "Adult",
+                                 "Adventure",
+                                 "Animation",
+                                 "Biography",
+                                 "Comedy",
+                                 "Documentary",
+                                 "Drama",
+                                 "Family",
+                                 "Fantasy",
+                                 "Film-Noir",
+                                 "History",
+                                 "Horror",
+                                 "Music",
+                                 "Musical",
+                                 "Mystery",
+                                 "Romance",
+                                 "Sci-Fi",
+                                 "Thriller",
+                                 "War",
+                                 "Western"),
+                  selected = "Action")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("plotOutputName")
+    )
   )
+)
+
+
+server <- function(input, output, session) {
+  
+  output$plotOutputName <- renderPlot({
+    plotThing %>%
+      ggplot(data = averageYearGenre_1, mapping = aes(x = year, y = average_score, color = genre_1)) +
+      geom_line()
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
