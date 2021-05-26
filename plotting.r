8a75e06a4f3cb9f496c190760645f29b6cd02a05
Dimitar Angelov <44362174+DMAgit@users.noreply.github.com>
2021-05-26 16:37:13 +0300

    Add files via upload

--- plotting.r
+++ plotting.r
@@ -3,15 +3,25 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(viridis)
+library(bslib)
 
-dataIMDB <- read_csv("D:\\Google Drive\\Uni\\Tilburg\\Semester 6\\Interactive Information Visualization\\Group Project\\dataPreprocessed.csv")
+dataIMDB <- read_csv("dataPreprocessed.csv")
 
 dataIMBDclean <- na.omit(dataIMDB)
 
-averageYearGenre_1 <- group_by(dataIMBDclean, year, genre_1) %>% 
-  summarize(average_score = mean(avg_vote)) 
+colnames(dataIMBDclean)[colnames(dataIMBDclean) == "year"] <- "Year"
+
+dataIMBDcleanLong <- dataIMBDclean %>%
+  pivot_longer(cols = starts_with("genre"), names_to = "genre_nr", values_to = "Genre") 
+
+averageYearAllGenre <- group_by(dataIMBDcleanLong, Year, Genre) %>% 
+  summarize(Score = mean(avg_vote)) 
 
 ui <- fluidPage(
+  
+  # Change the theme of Shiny
+  theme = bs_theme(bootswatch = "darkly"),
   
   # Application title
   titlePanel("IMDB Average Score"),
@@ -43,7 +53,7 @@ ui <- fluidPage(
                          selected = "Action"),
       sliderInput("range", 
                   label = "Choose a start and end year:",
-                  min = min(averageYearGenre_1$year), max = max(averageYearGenre_1$year), value = c(min(averageYearGenre_1$year), max(averageYearGenre_1$year)), sep = "",)
+                  min = min(averageYearAllGenre$Year), max = max(averageYearAllGenre$Year), value = c(min(averageYearAllGenre$Year), max(averageYearAllGenre$Year)), sep = "",)
       ),
     
     
@@ -57,23 +67,29 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   tempData <- reactive({
-    filter(averageYearGenre_1, genre_1 %in% input$genresGroup1 & year >= input$range[1] & year <= input$range[2])
+    filter(averageYearAllGenre, Genre %in% input$genresGroup1 & Year >= input$range[1] & Year <= input$range[2])
   })
   
   output$plotOutputName <- renderPlotly({
     ggplotly(
-      ggplot(data = tempData(), mapping = aes(x = year, y = average_score, color = genre_1)) +
-        geom_point(alpha=0.6, size=1) + 
+      tempData() %>%
+        ggplot(mapping = aes(x = Year, y = Score, color = Genre)) +
+        scale_color_viridis(option = "turbo", discrete=TRUE) +
+        geom_point(alpha=0.5, size=0.75) + 
         geom_smooth(se=FALSE) +
-        theme_minimal() +
+        theme_classic() +
         labs(
           x = "Year",
           y = "Average score",
-          color = "Gerne",
-          title = "The average score per Genre per Year",
+          color = "Genre",
+          title = "A Plot of Movies' Average Score per Genre per Year",
           subtitle = "Subtitle")
   )})
 }
 
 shinyApp(ui = ui, server = server)
+
+
+# change the dots to be off by default
+# figure out colors
 
